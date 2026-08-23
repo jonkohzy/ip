7227a0e87c8d56fc54866c2657030cf087d789a1
Jonathan Koh <jonkoh244@gmail.com>
2026-08-24 02:42:59 +0800

    Add Level-1 command loop
    Echo each user command and end the chatbot interaction when the user enters bye.

--- src/main/java/Jonk.java
+++ src/main/java/Jonk.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Jonk {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
         String name = "Jonk";
         String greeting = "Hello! I'm " + name + ".\nWhat can I do for you?";
@@ -9,12 +12,22 @@ public class Jonk {
                 + " _  | | | | |  \\| | ' / \n"
                 + "| |_| | |_| | |\\  | . \\ \n"
                 + " \\___/ \\___/|_| \\_|_|\\_\\";
-        
-        System.out.println(line);
-        System.out.println(banner);
-        System.out.println(greeting);
-        System.out.println(line);
-        System.out.println(bye);
-        System.out.println(line);
+
+        System.out.println("\t" + line);
+        System.out.println("\t" + banner);
+        System.out.println("\t" + greeting);
+        System.out.println("\t" + line);
+
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("\t" + line);
+            System.out.println("\t" + input);
+            System.out.println("\t" + line);
+            input = scanner.nextLine();
+        }
+
+        System.out.println("\t" + line);
+        System.out.println("\t" + bye);
+        System.out.println("\t" + line);
     }
 }
