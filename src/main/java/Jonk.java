be7f43a47f87ab729d2ec2a8ab83db4f6968adfe
Jonathan Koh <jonkoh244@gmail.com>
2026-08-23 23:03:08 +0800

    Level-0
    Rename the chatbot to Jonk and add the initial greeting and exit output.

---
+++ src/main/java/Jonk.java
@@ -0,0 +1,20 @@
+public class Jonk {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String name = "Jonk";
+        String greeting = "Hello! I'm " + name + ".\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        String banner = "     _  ___  _   _ _  __\n"
+                + "    | |/ _ \\| \\ | | |/ /\n"
+                + " _  | | | | |  \\| | ' / \n"
+                + "| |_| | |_| | |\\  | . \\ \n"
+                + " \\___/ \\___/|_| \\_|_|\\_\\";
+        
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+    }
+}
