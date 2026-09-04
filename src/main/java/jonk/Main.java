906212e0ba4f593f18c659b782125e87f0298ec3
Jonathan Koh <jonkoh244@gmail.com>
2026-09-04 17:49:59 +0800

    Implement Level-10 tasks

--- src/main/java/jonk/Main.java
+++ src/main/java/jonk/Main.java
@@ -1,20 +1,29 @@
 package jonk;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
- * Displays the Hello World JavaFX application.
+ * Displays the Jonk chatbot GUI using FXML.
  */
 public class Main extends Application {
+    private final Jonk jonk = new Jonk();
 
     @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+        AnchorPane mainLayout = fxmlLoader.load();
+        Scene scene = new Scene(mainLayout);
+
         stage.setScene(scene);
+        stage.setTitle("Jonk");
+        stage.setResizable(false);
+        fxmlLoader.<MainWindow>getController().setJonk(jonk);
         stage.show();
     }
 }
